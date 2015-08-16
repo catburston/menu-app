e1f0e6e9204197730c0d4cde2d02c0dd19637381
catburston <cat1788@gmail.com>
2015-08-16 18:40:34 +0200

    gemfile and seedsdb updated

--- Gemfile
+++ Gemfile
@@ -28,6 +28,9 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',        group: :development
 
+# seed_dump
+gem 'seed_dump', group: :development
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ db/seeds_old.rb
@@ -0,0 +1,60 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Rails.env == "development" || Rails.env == "test"
+  puts "Cleaning DB first ..."
+  Menu.destroy_all
+  Recipe.destroy_all
+  Ingredient.destroy_all
+end
+
+MENUS = 5
+
+puts "Creating Ingredients"
+ingredientsArray = ['allspice', 'baking powder', 'bay leaves', 'broccoli', 'brown sugar', 'bulghur', 'butter', 'capers', 'cardamom pods', 'carrot', 'capsicum', 'cauliflower', 'celery', 'cheddar', 'chilli powder', 'chilli sauce', 'chillies', 'cinnamon', 'cloves', 'coriander', 'coriander powder', 'creme fraiche', 'cumin seeds', 'curry leaves', 'dill', 'egg', 'eggplant', 'fennel', 'fetta', 'filo', 'flatbreads', 'flour', 'frozen peas', 'frozen spinach', 'garlic', 'ginger', 'goat cheese', 'gorgonzola', 'green olives', 'heavy cream', 'hokkien noodles', 'kasoori methi', 'leeks', 'lemon', 'lime', 'linguine', 'marjoram', 'milk', 'mint', 'mozzarella', 'mushroom', 'mustard seeds', 'nutmeg', 'olive oil', 'onions', 'oregano', 'orzo', 'panch phora', 'parmesan', 'parsley', 'pasta', 'pastry shell', 'pecorino', 'peppercorns', 'pinenuts', 'potatoes', 'red onion', 'red peppers', 'rice noodles', 'rocket', 'salted peanuts', 'scallions', 'shahi jeera', 'sour cream', 'soy sauce', 'spring onions', 'sriracha', 'star anise', 'swiss cheese', 'tarragon', 'tin lentils', 'tin tomatoes', 'tofu (fried)', 'tofu (hard)', 'tomato', 'tomato paste', 'tomato sauce ', 'tumeric', 'vegetable oil', 'vegetable stock', 'walnuts', 'white vinegar', 'yoghurt', 'zucchini']
+ingredient_sections = ['dairy', 'nuts', 'bread', 'fruit and veg', 'tinned goods', 'cleaning', 'market', 'frozen', 'deli', 'snacks', 'baking']
+
+(1..ingredientsArray.length-1).each do |index|
+  Ingredient.create!(name: ingredientsArray[index-1], shop_section: ingredient_sections.sample)
+end
+puts "#{ingredientsArray.length} ingredients created"
+
+puts "Creating Recipes"
+recipesArray = ['aloo gobi', 'baked eggplant pasta', 'baked leek and walnut penne', 'baked tomato pasta sauce', 'bechamel and roast veg', 'bowl o veg', 'broccoli chilli pasta', 'caponata', 'cauliflower cheese', 'dahl', 'fennel salad', 'fried rice', 'frittata', 'fritters (broccoli)', 'fritters (cauliflower)', 'fritters (zucchini)', 'grain salad', 'laksa soup', 'macaroni cheese', 'margarita pizza', 'mee goreng', 'mushroom and pesto pizza', 'mushroom quiche', 'muttar paneer', 'pad thai', 'pakora', 'palak gobi', 'pasta mozzarella salad', 'pumpkin soup', 'red pepper alfredo', 'red pepper soup', 'risotto (mushroom)', 'saag paneer', 'samosas', 'shepherd pie', 'spanakopita', 'tabbouleh', 'tex mex', 'veggie pasta', 'veggie soup', 'zucchini coconut curry']
+recipe_categories = ['pasta', 'rice', 'soup', 'salad', 'other', 'curry']
+recipe_types = ['schoolnight', 'potheavy', 'main', 'side', 'dessert']
+recipe_chefs = ['Cat', 'Tom']
+
+(1..recipesArray.length-1).each do |index|
+  Recipe.create!(name: recipesArray[index-1], recipe_type: recipe_types.sample, recipe_category: recipe_categories.sample, chef: recipe_chefs.sample)
+end
+allrecipes = Recipe.all.to_a
+puts "#{recipesArray.length} recipes created"
+
+
+puts "Creating Menus"
+(1..MENUS).each do |index|
+  Menu.create!(name: "Menu No. " + index.to_s)
+end
+
+menus = Menu.all.to_a
+menus.each do |m, index|
+  (3..6).each do
+    m.recipes << allrecipes.sample
+  end
+end
+puts "#{menus.length} menus created"
+
+puts "Creating Groceries"
+groceriesArray = ['woolwash', 'floor cleaner', 'laundry detergent', 'batteries', 'toilet paper', 'kitchen towel']
+grocery_sections = ['laundry', 'cleaning', 'other', 'personal hygiene']
+
+(1..groceriesArray.length-1).each do |index|
+  Grocery.create!(name: groceriesArray[index-1], section: grocery_sections.sample)
+end
+puts "#{groceriesArray.length} groceries created"
