592de9408d35b340ef0e81f2b53e7a70e15d1e98
catburston <cat1788@gmail.com>
2015-08-24 18:58:17 +0200

    remove third column from recipes/menus indexes

--- capitalise.rb
+++ capitalise.rb
@@ -5,3 +5,11 @@ end
 @ingredients.each do |i|
   i.update_attribute(:name, i.name.capitalize())
 end
+
+Ingredient.select(:shop_section).distinct
+x.each do |i|
+  y = Ingredient.where(shop_section: i.shop_section)
+  y.each do |ing|
+    puts ing.name
+  end
+end
