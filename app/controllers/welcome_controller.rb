7020acb1ef9be388c1807c64708709f89284414a
Cat Burston <cat1788@gmail.com>
2015-08-25 14:31:27 +0200

    redirect welcome to new menu page if no shopping list exists + vendor bundle

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,13 @@
 class WelcomeController < ApplicationController
   def index
-    @shopping_list = ShoppingList.last
-    ingredients = @shopping_list.ingredients
-    @ingredient_list = ingredients.group("ingredients.name").count
-    groceries = @shopping_list.groceries
-    @grocery_list = groceries.group("groceries.name").count
+    if !ShoppingList.all.empty?
+      @shopping_list = ShoppingList.last
+      ingredients = @shopping_list.ingredients
+      @ingredient_list = ingredients.group("ingredients.name").count
+      groceries = @shopping_list.groceries
+      @grocery_list = groceries.group("groceries.name").count
+    else
+      redirect_to new_menu_url
+    end
   end
 end
---
+++ vendor/bundle/ruby/2.2.0/gems/foundation-rails-5.4.0.0/lib/foundation/rails/version.rb
@@ -0,0 +1,5 @@
+module Foundation
+  module Rails
+    VERSION = "5.4.0.0"
+  end
+end
