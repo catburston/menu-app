4bfa99832607a6edb3d3d85b88ec1f84f5c6100e
Cat Burston <cat1788@gmail.com>
2015-12-13 09:39:00 +0100

    Revert "not sure what all this shit is"
    This reverts commit 7305cdddbdae4e533334f5b16c749939a393a789.

--- vendor/bundle/ruby/2.0.0/gems/pg-0.18.3/ext/siteconf20151102-2889-1w3v7i3.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20151102-2889-1s90m0f"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
